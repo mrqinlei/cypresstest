698924402276ac0409f3f3fb19f423247b126478
afei <qinlei@ezone.work>
2022-07-23 18:45:36 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 rm -rf ./cypress/results/*
+rm -rf ./cypress/screenshots/*
+rm -rf ./cypress/videos/*
 npm run cypress:run
 npx mochawesome-merge cypress/results/*.json > merge-report.json
 npx mochawesome-report-generator merge-report.json
