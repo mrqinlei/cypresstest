f7626474c838a3515a550bf7dea1a31fd5b79891
afei <373912677@qq.com>
2022-07-30 16:49:46 +0800

    update

--- cypress/e2e/repo/pushjavademo.sh
+++ cypress/e2e/repo/pushjavademo.sh
@@ -1,4 +1,6 @@
 cd cypress/e2e/repo/java-demo
+git config --global user.name "afei"
+git config --global user.email "373912677@qq.com"
 git remote rm origin
 git init
 git remote add origin http://ezone-test.work/ABC/uicreatecodedir/uicreatecoderepo.git
